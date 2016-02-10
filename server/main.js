0bb677030df92ece2354969afe79ac8bacc1b7ad
Manuel Gopar <manuelgop@gmail.com>
2016-02-10 12:05:23 -0600

    Segundo commit, algunos errores

--- server/main.js
+++ server/main.js
@@ -2,3 +2,28 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+var messages =[{
+    id: 1,
+    text: "Hola soy un mensaje",
+    author: "Manuel Gopar"
+  }]
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.status(200).send("Helow wordl");
+});
+
+io.on('connection', function (socket) {
+    console.log('Alguien se ha conectado con Sockets');
+    socket.emit('messages', messages);
+    socket.on('new-message', function (data) {
+        messages.push(data);
+
+        io.socket.emit('messages', messages);
+    });
+});
+
+server.listen(8080, function () {
+ console.log("Servidor corriendo en localhost:8080");
+});
